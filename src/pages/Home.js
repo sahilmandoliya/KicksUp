c1d200338f6da0d720df4467e0c7e9869293d2c4
sahilmandoliya <mandoliyasahil@gmail.com>
2022-07-20 17:30:35 +0530

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import { Container } from 'react-bootstrap';
 
 function Home() {
   return (
-    <Container className="justify-content-center center">Home</Container>
+    <Container className="justify-content-center center">
+      <div class="text-center"><h1>Welcome to KicksUp</h1></div>
+    </Container>
   )
 }
 
